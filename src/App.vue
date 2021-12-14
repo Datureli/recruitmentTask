961c92c5faefaa8e95b1f006a43cc7b9069a7f6a
Datureli <soleris1@gmail.com>
2021-12-14 19:08:38 +0100

    fix bug

--- src/App.vue
+++ src/App.vue
@@ -265,7 +265,7 @@ p {
   width: 200px;
   height: 200px;
   color: rgb(255, 255, 255);
-  background: rgb(66, 156, 66);
+  background: rgb(71, 167, 71);
   border: 2px solid rgb(0, 128, 0);
   text-align: center;
   justify-content: center;
